630a488ec05d15f0bc7d579189d3fdb4acfdac55
Eleanor Burgess <Eleanor.C.Burgess0940@gmail.com>
2020-03-11 09:15:26 -0400

    Created arrays of the variables

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input\
-var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var lowercase = "abcdefghijklmonpqrstuvwxyz"
-var numeric = "0123456789"
-var special = "!$%^&*()-=+[]{};#:@~,./<>?"
+function generatePassword()
+
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var special = ["!", "$", "%", "^", "&", "*", "(", ")", "-", "=", "+", "[", "]", "{", "}", ",", ";", "#", ":", "@", "~", ".", "/", "<", ">", "?"]
 
 var length = prompt("How many characters would you like to include?");
 
