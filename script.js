856080141b889c23d3ae53ad168dde3e24fd20ef
Eleanor Burgess <Eleanor.C.Burgess0940@gmail.com>
2020-03-14 13:04:44 -0400

    added commas between each variable in the array and wrote basic function to writePassword

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input\
-function generatePassword()
+function writePassword()
+var password = generatePassword()
+var passwordText = document.querySelector("#password");
 
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
