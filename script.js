e17e7713c67fce3558df081798e96e94f15d9549
Eleanor Burgess <Eleanor.C.Burgess0940@gmail.com>
2020-03-14 13:18:31 -0400

    Added a prompt for password length and uppercase letters

--- script.js
+++ script.js
@@ -16,3 +16,23 @@ var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 var special = ["!", "$", "%", "^", "&", "*", "(", ")", "-", "=", "+", "[", "]", "{", "}", ",", ";", "#", ":", "@", "~", ".", "/", "<", ">", "?"]
+
+var minimumCount = 0
+
+var passwordLength = prompt("How many characters do you want your password to be?")
+if (passwordLength < 8 || passwordLength > 128) {
+  alert("password length should be between 8 - 128, try again");
+  prompt("How many characters do you want your password to be?");
+}
+else {
+  alert("you have chosen a password length of " + passwordLength);
+}
+
+var passUppercase = confirm("Would you like to include uppercase characters?");
+if (passUppercase === true) {
+  alert("The password will include Uppercase letters");
+  minimumCount++;
+}
+else {
+}
+
